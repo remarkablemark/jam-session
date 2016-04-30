4acce7f953c43be3b53e256abff639f2dcb8e0a1
Mark <mark@remarkablemark.org>
2016-04-30 19:28:33 +0000

    Tidy app.js with comments

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,15 +10,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
+/**
+ * View engine setup.
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+/**
+ * Middleware.
+ */
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +28,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes.
+ */
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 app.use('/', routes);
 app.use('/users', users);
@@ -32,8 +45,9 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
+/**
+ * Error handlers.
+ */
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -56,5 +70,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+/**
+ * Export app.
+ */
 module.exports = app;
